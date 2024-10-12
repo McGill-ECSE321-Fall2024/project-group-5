8961061ea0b9d232f3be07c6c04006d516d8410b
alishamalik00 <alisioso0417@gmail.com>
2024-10-12 19:14:29 -0400

    updated PaymentInformation

--- GameStore-Backend/src/main/java/ca/mcgill/ecse321/gamestore/dao/PaymentInformationRepository.java
+++ GameStore-Backend/src/main/java/ca/mcgill/ecse321/gamestore/dao/PaymentInformationRepository.java
@@ -1,9 +1,18 @@
 package ca.mcgill.ecse321.gamestore.dao;
 
-import ca.mcgill.ecse321.gamestore.model.Account;
-
+import ca.mcgill.ecse321.gamestore.model.PaymentInformation;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AccountRepository extends CrudRepository<Account, Long> {
+public interface PaymentInformationRepository extends CrudRepository<PaymentInformation, Integer> {
+    
+
+    //find PaymentInformation by cardholder name
+    Iterable<PaymentInformation> findByCardholderName(String cardholderName);
+
+    //find  PaymentInformation by card type (Visa, Mastercard, etc.)
+    Iterable<PaymentInformation> findByCardType(PaymentInformation.CardType cardType);
+
+    //find PaymentInformation by customer account ID
+    Iterable<PaymentInformation> findByCustomerAccount_CustomerAccountId(int customerAccountId);
 
 }
