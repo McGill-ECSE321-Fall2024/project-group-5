8b2d947700db0237dfa9d068c9df9360173b0abb
alishamalik00 <alisioso0417@gmail.com>
2024-10-12 19:23:33 -0400

    updated cart repo

--- GameStore-Backend/src/main/java/ca/mcgill/ecse321/gamestore/dao/CartRepository.java
+++ GameStore-Backend/src/main/java/ca/mcgill/ecse321/gamestore/dao/CartRepository.java
@@ -1,9 +1,13 @@
 package ca.mcgill.ecse321.gamestore.dao;
 
-import ca.mcgill.ecse321.gamestore.model.Account;
-
+import ca.mcgill.ecse321.gamestore.model.Cart;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AccountRepository extends CrudRepository<Account, Long> {
+public interface CartRepository extends CrudRepository<Cart, Integer> {
 
+    //find all carts for a specific customer account by ID
+    Iterable<Cart> findByCustomerAccount_CustomerAccountId(int customerAccountId);
+
+    //find the cart for a specific order ID
+    Cart findByOrder_OrderId(int orderId);
 }
