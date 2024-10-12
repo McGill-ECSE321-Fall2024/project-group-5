e32026a17f1132b40461ccb3ac698a690027da04
alishamalik00 <alisioso0417@gmail.com>
2024-10-12 19:11:36 -0400

    updated orders repository

--- GameStore-Backend/src/main/java/ca/mcgill/ecse321/gamestore/dao/OrderRepository.java
+++ GameStore-Backend/src/main/java/ca/mcgill/ecse321/gamestore/dao/OrderRepository.java
@@ -1,9 +1,20 @@
 package ca.mcgill.ecse321.gamestore.dao;
 
-import ca.mcgill.ecse321.gamestore.model.Account;
-
+import ca.mcgill.ecse321.gamestore.model.Order;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AccountRepository extends CrudRepository<Account, Long> {
-
+public interface OrderRepository extends CrudRepository<Order, Integer> {
+    
+    //find orders by payment status
+    Iterable<Order> findByIsPaid(boolean isPaid);
+    
+    //find orders by delivery status
+    Iterable<Order> findByDeliveryStatus(boolean deliveryStatus);
+    
+    //find orders by customer account ID
+    Iterable<Order> findByCustomerAccount_CustomerAccountId(int customerAccountId);
+    
+    //find orders by promotion code
+    Iterable<Order> findByPromotionCode(String promotionCode);
+    
 }
