15c2db6f1d540ce6d8c8ed83e2afab6366f31546
anafloarea <ana-maria.floarea@mail.mcgill.ca>
2024-11-06 12:12:06 -0500

    dto, controller, service

--- GameStore-Backend/src/main/java/ca/mcgill/ecse321/gamestore/service/AddressService.java
+++ GameStore-Backend/src/main/java/ca/mcgill/ecse321/gamestore/service/AddressService.java
@@ -1,8 +1,6 @@
 package ca.mcgill.ecse321.gamestore.service;
 
-import java.sql.Date;
-import java.time.LocalDate;
-
+import java.util.Optional;  // Import for Optional
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +10,61 @@ import jakarta.transaction.Transactional;
 
 @Service
 public class AddressService {
+
     @Autowired
     private AddressRepository addressRepository;
 
+    public Address createAddress(Address address) {
+        return addressRepository.save(address);
+    }
+
+    public Address getAddressById(int id) {
+        return addressRepository.findById(id).orElse(null);
+    }
+
+    public Iterable<Address> getAllAddresses() {
+        return addressRepository.findAll();
+    }
+
+    public Iterable<Address> getAddressesByCity(String city) {
+        return addressRepository.findByCity(city);
+    }
+
+    public Iterable<Address> getAddressesByProvince(String province) {
+        return addressRepository.findByProvince(province);
+    }
+
+    public Iterable<Address> getAddressesByCountry(String country) {
+        return addressRepository.findByCountry(country);
+    }
+
+    public Iterable<Address> getAddressesByPostalCode(String postalCode) {
+        return addressRepository.findByPostalCode(postalCode);
+    }
+
+    public Iterable<Address> getAddressesByCustomerId(int customerId) {
+        return addressRepository.findByCustomerAccount_Id(customerId);
+    }
+
+    public Address updateAddress(int id, Address updatedAddress) {
+        Optional<Address> existingAddress = addressRepository.findById(id);
+        if (existingAddress.isPresent()) {
+            Address address = existingAddress.get();
+            address.setAddress(updatedAddress.getAddress());
+            address.setCity(updatedAddress.getCity());
+            address.setProvince(updatedAddress.getProvince());
+            address.setCountry(updatedAddress.getCountry());
+            address.setPostalCode(updatedAddress.getPostalCode());
+            return addressRepository.save(address);
+        }
+        return null;
+    }
+
+    public boolean deleteAddress(int id) {
+        if (addressRepository.existsById(id)) {
+            addressRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
