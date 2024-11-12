902b32aabd7dc68722b38fb564a174f8c0fe4e76
alishamalik00 <alisioso0417@gmail.com>
2024-11-12 00:13:19 -0500

    updated SA res

--- GameStore-Backend/src/main/java/ca/mcgill/ecse321/gamestore/dto/StaffAccountResponseDto.java
+++ GameStore-Backend/src/main/java/ca/mcgill/ecse321/gamestore/dto/StaffAccountResponseDto.java
@@ -3,26 +3,18 @@ package ca.mcgill.ecse321.gamestore.dto;
 import ca.mcgill.ecse321.gamestore.model.StaffAccount;
 
 public class StaffAccountResponseDto {
-    //priavte fields
     private int id;
-    private String name;
     private String username;
+    private String name;
 
-    public StaffAccountResponseDto(StaffAccount model) {
-        this.id = model.getId();
-        this.name = model.getName();
-        this.username = model.getUsername();
+    public StaffAccountResponseDto(StaffAccount staffAccount) {
+        this.id = staffAccount.getId();
+        this.username = staffAccount.getUsername();
+        this.name = staffAccount.getName();
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
+    // Getters only (response DTOs should be immutable)
+    public int getId() { return id; }
+    public String getUsername() { return username; }
+    public String getName() { return name; }
 }
