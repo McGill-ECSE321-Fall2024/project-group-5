f6df99b3586b2e780b91a18dfa15b8812d0e71db
alishamalik00 <alisioso0417@gmail.com>
2024-11-11 23:55:16 -0500

    updated review response

--- GameStore-Backend/src/main/java/ca/mcgill/ecse321/gamestore/dto/ReviewResponseDto.java
+++ GameStore-Backend/src/main/java/ca/mcgill/ecse321/gamestore/dto/ReviewResponseDto.java
@@ -2,20 +2,33 @@ package ca.mcgill.ecse321.gamestore.dto;
 
 import ca.mcgill.ecse321.gamestore.model.Review;
 
+import java.sql.Date;
+
 public class ReviewResponseDto {
     private int id;
-    private String name;
+    private Date date;
+    private String description;
+    private int likeCount;
+    private int dislikeCount;
+    private float rating;
+    private boolean employeeReviewed;
 
-    public ReviewResponseDto(Review model) {
-        this.id = model.getId();
-        this.name = model.getName();
+    public ReviewResponseDto(Review review) {
+        this.id = review.getId();
+        this.date = review.getDate();
+        this.description = review.getDescription();
+        this.likeCount = review.getLikeCount();
+        this.dislikeCount = review.getDislikeCount();
+        this.rating = review.getRating();
+        this.employeeReviewed = review.isEmployeeReviewed();
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public String getName()  {
-        return name;
-    }
+    // Getters only
+    public int getId() { return id; }
+    public Date getDate() { return date; }
+    public String getDescription() { return description; }
+    public int getLikeCount() { return likeCount; }
+    public int getDislikeCount() { return dislikeCount; }
+    public float getRating() { return rating; }
+    public boolean isEmployeeReviewed() { return employeeReviewed; }
 }
