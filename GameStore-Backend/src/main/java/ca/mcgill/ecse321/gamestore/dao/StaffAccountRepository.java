e41f20571f75105727e17ceb82507794220eae1c
alishamalik00 <alisioso0417@gmail.com>
2024-11-11 22:03:30 -0500

    updated staff account repo

--- GameStore-Backend/src/main/java/ca/mcgill/ecse321/gamestore/dao/StaffAccountRepository.java
+++ GameStore-Backend/src/main/java/ca/mcgill/ecse321/gamestore/dao/StaffAccountRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.gamestore.model.StaffAccount;
 
 public interface StaffAccountRepository extends CrudRepository<StaffAccount, Integer> {
-    // checks by username
+    // checks by usernames
     boolean existsStaffAccountByUsername(String username);
 
     StaffAccount findStaffAccountByUsername(String username);
