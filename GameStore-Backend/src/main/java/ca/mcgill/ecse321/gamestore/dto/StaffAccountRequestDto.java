29a46fa5a1d78c6d129020ffe76186e57db626c0
alishamalik00 <alisioso0417@gmail.com>
2024-11-12 00:12:32 -0500

    updated SA req

--- GameStore-Backend/src/main/java/ca/mcgill/ecse321/gamestore/dto/StaffAccountRequestDto.java
+++ GameStore-Backend/src/main/java/ca/mcgill/ecse321/gamestore/dto/StaffAccountRequestDto.java
@@ -1,35 +1,15 @@
 package ca.mcgill.ecse321.gamestore.dto;
 
 public class StaffAccountRequestDto {
-    private String name;
     private String username;
-    private String password; // Avoid exposing passwords unnecessarily
+    private String password;
+    private String name;
 
-    public StaffAccountRequestDto() {
-        
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    // Getters and Setters
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 }
