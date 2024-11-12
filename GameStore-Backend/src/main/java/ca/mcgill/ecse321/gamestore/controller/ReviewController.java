dad780604f25341abd14f646f96372f033a2d6cd
alishamalik00 <alisioso0417@gmail.com>
2024-11-11 23:45:22 -0500

    updated controller

--- GameStore-Backend/src/main/java/ca/mcgill/ecse321/gamestore/controller/ReviewController.java
+++ GameStore-Backend/src/main/java/ca/mcgill/ecse321/gamestore/controller/ReviewController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class ReviewController {
+public class ReviewController  {
 
     @Autowired
     private ReviewService reviewService;
