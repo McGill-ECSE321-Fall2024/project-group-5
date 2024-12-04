94f72cd82b1045461c33f2192714ca86f371c2f9
anafloarea <ana-maria.floarea@mail.mcgill.ca>
2024-12-04 09:24:04 -0500

    fixing router

--- GameStore-Frontend/src/router/index.js
+++ GameStore-Frontend/src/router/index.js
@@ -1,16 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router';
-
 import OrderView from '../views/OrderView.vue'; // Import the new component
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'home', // You can change this to 'home' or whatever you prefer
+      component: OrderView, // You can change this to the component you want to show on the home page
+    },
+    {
       path: '/order',
       name: 'order',
       component: OrderView,
-    }
-     // Add the new route
+    },
+    // Add other routes as needed
   ],
 });
 
